dafa2fba0b8d7b947f7e954c7e11a7d8ce12f3be
Daniel Ekwuazi <danielekwuazi@gmail.com>
2016-08-18 22:31:50 +0700

    Create objcreation.js

---
+++ objcreation.js
@@ -0,0 +1,14 @@
+
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 1;
+};
+
+// Only change code below this line.
+
+var MotorBike = function() {
+      this.wheels = 1;
+      this.engines = 1;
+      this.seats = 1;
+};
