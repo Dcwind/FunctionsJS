d9aeff45b4f22d6cf24a681d5099ed93ecf0ce7a
Daniel Ekwuazi <danielekwuazi@gmail.com>
2016-09-09 10:06:52 +0700

    Create mutation.js
    checks if the target contains the test.

---
+++ mutation.js
@@ -0,0 +1,14 @@
+
+function mutation(arr) {
+  var test = arr[1].toLowerCase();
+  var target = arr[0].toLowerCase();
+  
+  for(var i = 0;i < arr[1].length; i++){
+    if (target.indexOf(test[i]) < 0){
+        return false;
+        }
+  }
+  return true;
+}
+
+mutation(["hello", "hey"]);
