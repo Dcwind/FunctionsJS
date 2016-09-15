d546d23401afa2c9cbdc0adaafe120a89bbbce52
Daniel Ekwuazi <danielekwuazi@gmail.com>
2016-09-15 22:00:35 +0700

    Update speed.js

--- speed.js
+++ speed.js
@@ -10,3 +10,19 @@ else {
     console.log("Drive safe");
 
 }
+
+// Check if the user is ready to play!
+
+confirm("Ready to play?");
+var age = prompt("What's your age");
+
+if (age < 13){
+    console.log("You are allowed to play but I take no responsibility");
+}else{
+    console.log("Play on!");
+}
+console.log("You are at a Justin Bieber concert, and you hear this lyric 'Lace my shoes off, start racing.'");
+
+console.log("Suddenly, Bieber stops and says, 'Who wants to race me?'");
+
+var userAnswer = prompt('Do you want to race Bieber on stage?"');
