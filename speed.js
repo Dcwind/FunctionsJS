4737eaacb3cb759b80644b038d6c6f4729ec1878
Daniel Ekwuazi <danielekwuazi@gmail.com>
2016-09-15 22:01:47 +0700

    Update speed.js

--- speed.js
+++ speed.js
@@ -3,7 +3,7 @@ var speed = 65;
 // Complete the condition in the ()s on line 4
 if (speed > 80 ) {
 	// Use console.log() to print "Slow down"
-	console.log("slow down");
+	console.log("slow down mofo");
 } 
 else {
 	// Use console.log() to print "Drive safe"
