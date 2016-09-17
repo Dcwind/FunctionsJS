109f7b2e09c449ce3f51ed6cd9198f862adad30a
Daniel Ekwuazi <danielekwuazi@gmail.com>
2016-09-17 19:26:09 +0700

    Update speed.js

--- speed.js
+++ speed.js
@@ -26,4 +26,16 @@ if (quarter(12) % 3 === 0 ) {
   console.log("The statement is false");
 }
 
+// get sleep :)
+var sleepCheck = function (numHours){
+    if(numHours >= 8){
+        return "You're getting plenty of sleep! Maybe even too much!";
+    }
+    else{
+        return "Get some more shut eye!";
+    }
+}  
 
+sleepCheck(10);
+sleepCheck(5);
+sleepCheck(8);
