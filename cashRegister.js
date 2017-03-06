e68bb6a26f3092c52220904dcf2bd8fe1c58d3c3
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-03-06 10:49:20 +0700

    Update cashRegister.js

--- cashRegister.js
+++ cashRegister.js
@@ -1,3 +1,16 @@
+// create a constructor for the StaffMember class
+function StaffMember (name,discountPercent) {
+    this.name = name;
+    this.discountPercent = discountPercent;
+}
+
+var sally = new StaffMember("Sally",5);
+var bob = new StaffMember("Bob",10);
+
+//Create a StaffMember for yourself called me
+
+var me = new StaffMember("me",20);
+
 var cashRegister = {
     total:0,
     //Dont forget to add your property
