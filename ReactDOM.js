778ff48a0a48ecdc4da184cf9b2e526d260570b9
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-03-08 19:54:08 +0700

    Update ReactDOM.js

--- ReactDOM.js
+++ ReactDOM.js
@@ -1,18 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var pi = (
-  <div>
-    <h1>
-      PI, YALL!
-    </h1>
-    <p>
-      {Math.PI.toFixed(20)}
-    </p>
-  </div>
+function makeDoggy (e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+var kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" onClick={makeDoggy}/>
 );
 
-ReactDOM.render(
-	pi,
-	document.getElementById('app')
-);
+ReactDOM.render(kitty,document.getElementById('app'));
