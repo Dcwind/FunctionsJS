b1a1dd0bd3dba86abf1dddb02d142619027cdb7b
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-03-09 06:32:54 +0700

    Update ReactDOM.js

--- ReactDOM.js
+++ ReactDOM.js
@@ -15,3 +15,27 @@ var kitty = (
 );
 
 ReactDOM.render(kitty,document.getElementById('app'));
+
+// including if/else statement
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+if (user.age >= drinkingAge) {
+  var message = (
+    <h1>
+      Hey, check out this alcoholic beverage!
+    </h1>
+  );
+} else {
+  var message = (
+    <h1>
+      Hey, check out these earrings I got at Claire's!
+    </h1>
+  );
+}
+
+ReactDOM.render(
+  message, 
+  document.getElementById('app')
+);
