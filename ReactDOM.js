1923c16ad55dd2322a4fd35135f4124a2dd98af2
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-04-01 11:04:29 +0700

    Update ReactDOM.js

--- ReactDOM.js
+++ ReactDOM.js
@@ -135,4 +135,18 @@ var ProfilePage = React.createClass({
   }
 });
 
+// this.props
+var React = require('react');
+var ReactDOM = require('react-dom');
 
+var Greeting = React.createClass({
+  render: function () {
+    return <h1>Hi there, {this.props.firstName}!</h1>;
+  }
+});
+
+// ReactDOM.render goes here:
+ReactDOM.render(
+  <Greeting firstName='Luke' />, 
+  document.getElementById('app')
+);
