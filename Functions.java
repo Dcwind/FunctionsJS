3d6ad9607dfa5dd8e5c0cbca509e1f1cafaecc48
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-05-07 21:42:51 +0700

    Update Functions.java

--- Functions.java
+++ Functions.java
@@ -119,6 +119,10 @@ public class Temperatures {
 			System.out.println(temperature);
 		}
 	HashMap<String, Integer> restaurantMenu = new HashMap<String, Integer>();
+	restaurantMenu.put("Turkey Burger", 13);
+    	restaurantMenu.put("Naan Pizza", 11);
+    	restaurantMenu.put("Cranberry Kale Salad", 10);
+
 
     }
 }
