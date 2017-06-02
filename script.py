646a7cc844023fcdbce07b693acb9c291bcb9edd
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-02 23:41:13 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,11 +1,8 @@
 # Create the areas list
 areas = ["hallway", 11.25, "kitchen", 18.0, "living room", 20.0, "bedroom", 10.75, "bathroom", 9.50]
 
-# Print out second element from areas
+# Sum of kitchen and bedroom area: eat_sleep_area
+eat_sleep_area = areas[3] + areas[-3]
 
-
-# Print out last element from areas
-
-
-# Print out the area of the living room
-
+# Print the variable eat_sleep_area
+print(eat_sleep_area)
