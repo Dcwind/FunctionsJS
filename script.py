155d66279f93c93a4da496e229d2edefab5a995f
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-04 21:51:42 +0700

    Update script.py

--- script.py
+++ script.py
@@ -34,3 +34,16 @@ areas_copy[0] = 5.0
 
 # Print areas
 print(areas)
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full,reverse=1)
+
+# Print out full_sorted
+print(full_sorted)
