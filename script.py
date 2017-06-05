a928e32b3aaf94f162ef93a7c9cb1d852a317b84
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-05 23:06:48 +0700

    Update script.py

--- script.py
+++ script.py
@@ -67,3 +67,19 @@ print(room.count("o"))
 print(areas.index(20.0))
 # Reverse the orders of the elements in areas
 areas.reverse()
+
+# Definition of radius
+r = 0.43
+
+# Import the math package
+import math
+
+# Calculate C
+C = 2 * math.pi * r
+
+# Calculate A
+A = math.pi * r**2
+
+# Build printout
+print("Circumference: " + str(C))
+print("Area: " + str(A))
