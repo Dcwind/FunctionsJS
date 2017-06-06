b924b37b4e435f08b8ad3f48057585055fe92795
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-06 18:06:41 +0700

    Update script.py

--- script.py
+++ script.py
@@ -95,3 +95,15 @@ dist = r * radians(12)
 
 # Print out dist
 print(dist)
+
+# Create list baseball
+baseball = [180, 215, 210, 210, 188, 176, 209, 200]
+
+# Import the numpy package as np
+import numpy as np
+
+# Create a numpy array from baseball: np_baseball
+np_baseball = np.array(baseball)
+
+# Print out type of np_baseball
+print(type(np_baseball))
