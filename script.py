787b9b630afb0b0144e46432959879417d8e6c01
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-06 21:48:29 +0700

    Update script.py

--- script.py
+++ script.py
@@ -133,3 +133,13 @@ print(light)
 
 # Print out BMIs of all baseball players whose BMI is below 21
 print(bmi[light])
+
+# Store weight and height lists as numpy arrays
+np_weight = np.array(weight)
+np_height = np.array(height)
+
+# Print out the weight at index 50
+print(np_weight[50])
+
+# Print out sub-array of np_height: index 100 up to and including index 110
+print(np_height[100:111])
