76e2b99eae3e3c3a545cb371ac2bcb3a675dcf92
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-07 22:50:00 +0700

    Update script.py

--- script.py
+++ script.py
@@ -161,3 +161,12 @@ print(type(np_baseball))
 
 # Print out the shape of np_baseball
 print(np_baseball.shape)
+
+# Create np_height from np_baseball
+np_height = np_baseball[:,0]
+
+# Print out the mean of np_height
+print(np.mean(np_height))
+
+# Print out the median of np_height
+print(np.median(np_height))
