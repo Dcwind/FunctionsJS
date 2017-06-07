077161cd9f1a657f051f516dd81a088b5bc49126
Daniel Ekwuazi <danielekwuazi@gmail.com>
2017-06-07 23:00:39 +0700

    Update script.py

--- script.py
+++ script.py
@@ -170,3 +170,11 @@ print(np.mean(np_height))
 
 # Print out the median of np_height
 print(np.median(np_height))
+
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:,0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:,0],np_baseball[:,1])
+print("Correlation: " + str(corr))
